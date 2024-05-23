e7495cba17764469cf766eaf3b2e730e447c25a8
DongjuMun <150094637+DongjuMun@users.noreply.github.com>
2024-05-23 02:12:07 -0600

    Update Doctor.h

--- Doctor.h
+++ Doctor.h
@@ -15,13 +15,13 @@ private:
 	int servicio;
 public:
 	Doctor();
-	Doctor(string _userId, string _password); //overloading
-	Doctor(string _userId, string _password, DatoDePaciente _datoDePaciente, int _servicio); //overloading
+	Doctor(string _userId, string _password); //sobrecarga
+	Doctor(string _userId, string _password, DatoDePaciente _datoDePaciente, int _servicio); //sobrecarga
 	void setServicio(int _servicio);
 	int getServicio();
 	vector<DatoDePaciente>& getDatoDePaciente();
 	void agregarPaciente(DatoDePaciente _datoDePaciente);
-	bool checaLogInfo(string _userId, string _password, int _servicio);
+	bool checaLogInfo(string _userId, string _password, int _servicio); //sobreescritura
 	void guradarInfoDePacientes(string _userId, string _password, int _servicio, DatoDePaciente _datoDePaciente);
 	void guradarInfoDePago(string _userId, string _password, int _servicio, DatoDePaciente _datoDePaciente);
 };
