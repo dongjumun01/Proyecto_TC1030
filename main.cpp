998e48c58658cb36def915115ddede05498e2980
DongjuMun <150094637+DongjuMun@users.noreply.github.com>
2024-05-30 04:02:39 -0600

    Update main.cpp
    avance 3

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
-﻿#include "Doctor.h"
+#include "Hospital.h"
+#include "Empleado.h"
+#include "Doctor.h"
 #include "Administrativo.h"
+#include "DatoDePaciente.h"
 
 int main()
 {
-	int opcion = 0;
+	Hospital hospital; //Carga la info de los empleados
 	string userId = "";
 	string password = "";
-	bool exit = false;
+
 	//crea 3 varaiables para recibir los datos que ingresa el usario.
 	string stringValue = "";
 	int intValue = 0;
 	bool boolValue = false;
+
 	Doctor doctor;
 	int servicio = 0;
 	DatoDePaciente dato_P;
+	bool arr[5];
+
 	Administrativo administrativo;
 	int numDePaciente = 0;
+
+	int opcion = 0;
+	bool exit = false;
 	bool exit_opcion1 = false;
 	bool exit_opcion2 = false;
 
@@ -32,21 +41,25 @@ int main()
 			{				
 				cout << "Ingresa tu id: ";
 				cin >> userId;
-				cout << "Ingresa tu contraseña: ";
+				cout << "Ingresa tu contrasena: ";
 				cin >> password;
-				//guardar el servicio del doctor.
-				cout << "Ingresa en que servicio perteneces (1: , 2: , 3: , 4: , 5: , 6: ): ";
+				cout << "Ingresa en que servicio perteneces (1: Cirugia General, 2: Gineco-Obstetricia, 3: Medicina Interna, 4: Pediatria, 5: Urgencias, 6: Medicina familiar): ";
 				cin >> servicio;
+				
 				//checa que si está en el sistema
-				if (doctor.checaLogInfo(userId, password, servicio)) //overriding
+				if (hospital.buscaEmpleado(userId, password)) //overriding
 				{
+					//imprime la info de el/la doctor/a
+					cout << hospital.getEmpleado(hospital.getIndex())->to_string(); 
+					//guarda el nombre de el/la doctor/a del objecto hosptial.
+					dato_P.setNombreDoc(hospital.getEmpleado(hospital.getIndex())->getNombre());
 					//los datos de paciente
 					//guarda el nombre de paciente.
 					cout << "Ingresa el nombre de paciente: ";
 					cin >> stringValue;
-					dato_P.setNombre(stringValue);
+					dato_P.setNombrePac(stringValue);
 					//guarda el sexo del paciente.
-					cout << "Ingresa el sexo de paciente (M/F): ";
+					cout << "Ingresa el sexo de paciente (m/f): ";
 					cin >> stringValue;
 					dato_P.setSexo(stringValue);
 					//guarda la fecha de nacimiento del paciente.
@@ -62,15 +75,54 @@ int main()
 					cin >> stringValue;
 					dato_P.setDiagnosis(stringValue);
 					//guarda los estudios que se realizaron.
-					//usando arreglo
-					// 
+					cout << "¿Se realizo el analisis de sangre? (Y/N): ";
+					cin >> stringValue;
+					if (stringValue == "Y") { arr[0] = true; }
+					else { arr[0] = false; }
+					cout << "¿Se realizo radiografia? (Y/N): ";
+					cin >> stringValue;
+					if (stringValue == "Y") { arr[1] = true; }
+					else { arr[1] = false; }
+					cout << "¿Se realizo ultrasonido? (Y/N): ";
+					cin >> stringValue;
+					if (stringValue == "Y") { arr[2] = true; }
+					else { arr[2] = false; }
+					cout << "¿Se realizo tomografia? (Y/N): ";
+					cin >> stringValue;
+					if (stringValue == "Y") { arr[3] = true; }
+					else { arr[3] = false; }
+					cout << "¿Se realizo resonancia magnetica? (Y/N): ";
+					cin >> stringValue;
+					if (stringValue == "Y") { arr[4] = true; }
+					else { arr[4] = false; }
+					dato_P.setEstudio(arr);
 					//guarda el sintoma de paciente.
-					cout << "Ingresa el síntoma de paciente: ";
+					cout << "Ingresa el sintoma de paciente: ";
 					cin >> stringValue;
 					dato_P.setSintoma(stringValue);
 					//guarda los tratamientos que se realizaron.
 					//usando arreglo
-					// 
+					cout << "¿Se uso adrenalina? (Y/N): ";
+					cin >> stringValue;
+					if (stringValue == "Y") { arr[0] = true; }
+					else { arr[0] = false; }
+					cout << "¿Se uso dopamina? (Y/N): ";
+					cin >> stringValue;
+					if (stringValue == "Y") { arr[1] = true; }
+					else { arr[1] = false; }
+					cout << "¿Se uso dobutamina? (Y/N): ";
+					cin >> stringValue;
+					if (stringValue == "Y") { arr[2] = true; }
+					else { arr[2] = false; }
+					cout << "¿Se uso metilprednisolona? (Y/N): ";
+					cin >> stringValue;
+					if (stringValue == "Y") { arr[3] = true; }
+					else { arr[3] = false; }
+					cout << "¿Se uso salbutamol? (Y/N): ";
+					cin >> stringValue;
+					if (stringValue == "Y") { arr[4] = true; }
+					else { arr[4] = false; }
+					dato_P.setTratamiento(arr);
 					//guarda la fecha inicio.
 					cout << "Ingresa la fecha inicio (DD/MM/AA): ";
 					cin >> stringValue;
@@ -80,31 +132,29 @@ int main()
 					cin >> stringValue;
 					dato_P.setFechaFin(stringValue);
 					//se guarda el login informacion del doctor con el dato de paciente.
-					doctor.setUserId(userId);
-					doctor.setPassword(password);
-					doctor.setServicio(servicio);
-					doctor.agregarPaciente(dato_P);
+					doctor.setUserId(userId); 
+					doctor.setPassword(password); 
+					doctor.setServicio(servicio);  
+					doctor.agregarPaciente(dato_P); 
 					numDePaciente++; //para saber cuantos pacinetes lleva el doctor.
 					cout << "¡Dato guardado exitosamente!" << endl;
 
 					boolValue = true;
 					while (boolValue)
 					{
-						cout << "¿Deseas guardar más datos de paciente? (Y/N): ";
+						cout << "¿Deseas guardar mas datos de paciente? (Y/N): ";
 						cin >> stringValue;
 						if (stringValue == "Y")
 						{
-							cout << "Elegiste Y. Continua guardando más datos." << endl;
+							cout << "Elegiste Y. Continua guardando mas datos." << endl;
 							boolValue = false;
 						}
 						else if (stringValue == "N")
 						{
 							cout << "Elegiste N. Saliendo al menu..." << endl;
-							for (int i = 0; i < numDePaciente; i++)
-							{
-								doctor.guradarInfoDePacientes(doctor.getUserId(), doctor.getPassword(), doctor.getServicio(), doctor.getDatoDePaciente().at(i)); //guarda la informacion del paciente en un archivo de texto acecdiendo a ith miembro del vector. Tambien se guarda el id del paciente.
-								doctor.guradarInfoDePago(doctor.getUserId(), doctor.getPassword(), doctor.getServicio(), doctor.getDatoDePaciente().at(i)); //guarda la informacion de pago del paciente en un archivo de texto acecdiendo a ith miembro del vector. Tambien se guarda el id del Paciente.
-							}
+							doctor.guradarInfoDePacientes(doctor.getUserId(), doctor.getServicio(), numDePaciente, doctor.getDatoDePaciente()); //guarda la informacion del paciente en un archivo de texto acecdiendo a ith miembro del vector. Tambien se guarda el id del paciente.
+							doctor.guradarInfoDePago(doctor.getUserId(), numDePaciente, doctor.getDatoDePaciente()); //guarda la informacion de pago del paciente en un archivo de texto acecdiendo a ith miembro del vector. Tambien se guarda el id del Paciente.
+							
 							boolValue = false;
 							exit_opcion1 = true;
 							numDePaciente = 0;
@@ -145,10 +195,13 @@ int main()
 			{
 				cout << "Ingresa tu id: ";
 				cin >> userId;
-				cout << "Ingresa tu contraseña: ";
+				cout << "Ingresa tu contrasena: ";
 				cin >> password;
-				if (administrativo.checaLogInfo(userId, password)) //Si el user id y la contraseña están en el sistema. overriding
+				if (hospital.buscaEmpleado(userId, password)) //Si el user id y la contraseña están en el sistema. overriding
 				{
+					//imprime la info del empleado
+					cout << hospital.getEmpleado(hospital.getIndex())->to_string();
+					//guarda la info del usario en el objeto administrativo
 					administrativo.setUserId(userId);
 					administrativo.setPassword(password);
 					boolValue = true;
@@ -160,15 +213,16 @@ int main()
 						if (administrativo.checaIdDePaciente(stringValue)) //Si el id de paciente existe en el sistema,
 						{
 							administrativo.setIdDePaciente(stringValue);
-							cout << "El monto total de paciente es " << administrativo.cargaMontoTotal(administrativo.getIdDePaciente()) << " (mxn)." << endl;
+							cout << "El monto total de paciente es " << administrativo.getMontoTotal() << " (mxn)." << endl;
 							while (boolValue)
 							{
 								cout << "¿Se ha realizado el pago? (Y/N): ";
 								cin >> stringValue;
 								if (stringValue == "Y")
 								{
-									cout << "Se está gurando el cambio..." << endl;
-									administrativo.guardaInfoDeHaPagado(administrativo.getUserId(), administrativo.getPassword(), administrativo.getIdDePaciente(), administrativo.getHaPagado());
+									administrativo.setHaPagado(true);
+									cout << "Se esta gurando el cambio..." << endl;
+									administrativo.guardaInfoDeHaPagado(administrativo.getUserId(), administrativo.getIdDePaciente(), administrativo.getHaPagado(), administrativo.getMontoTotal());
 									cout << "¡Guradado exitosamente!" << endl;
 									boolValue = false; //sale de este ciclo y del ciclo afuera.
 									cout << "Saliendo al menu..." << endl;
@@ -192,7 +246,7 @@ int main()
 							cout << "Ingreso incorrecto." << endl;
 							while (boolValue)
 							{
-								cout << "¿Deseas terminar la acción? (Y/N): ";
+								cout << "¿Deseas terminar la accion? (Y/N): ";
 								cin >> stringValue;
 								if (stringValue == "Y")
 								{
