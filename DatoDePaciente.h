82b83b1995f327e213964ffa1e44a932494d9ba9
DongjuMun <150094637+DongjuMun@users.noreply.github.com>
2024-05-30 04:07:39 -0600

    Update DatoDePaciente.h
    avance 3

--- DatoDePaciente.h
+++ DatoDePaciente.h
@@ -3,37 +3,41 @@
 #define DATODEPACIENTE_H
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 class DatoDePaciente
 {
 private:
-	string nombre;
+	string nombre_doc;
+	string nombre_pac;
 	string sexo;
 	string fechaDeNacimiento;
 	int numDeCama;
 	string diagnosis;
-	bool estudio[10];
+	bool estudio[5];
 	string sintoma;
-	bool tratamiento[10];
+	bool tratamiento[5];
 	string fechaInicio;
 	string fechaFin;
 
 public:
 	DatoDePaciente();
-	DatoDePaciente(string _nombre, string _sexo, string _fechaDeNacimiento, int _numDeCama, string _diagnosis, bool _estudio[], string _sintoma, bool _tratamiento[], string _fechaInicio, string _fechaFin); //overloading
-	void setNombre(string _nombre);
+	DatoDePaciente(string _nombreDoc, string _nombrePac, string _sexo, string _fechaDeNacimiento, int _numDeCama, string _diagnosis, bool _estudio[], string _sintoma, bool _tratamiento[], string _fechaInicio, string _fechaFin); //overloading
+	void setNombreDoc(string _nombreDoc);
+	void setNombrePac(string _nombrePac);
 	void setSexo(string _sexo);
 	void setFechaDeNacimiento(string _fechaDeNacimiento);
 	void setNumDeCama(int _numDeCama);
 	void setDiagnosis(string _diagnosis);
-	void setEstudio();
+	void setEstudio(bool _arr[]);
 	void setSintoma(string _sintoma);
-	void setTratamiento();
+	void setTratamiento(bool _arr[]);
 	void setFechaInicio(string _fechaInicio);
 	void setFechaFin(string _fechaFin);
-	string getNombre();
+	string getNombreDoc();
+	string getNombrePac();
 	string getSexo();
 	string getFechaDeNacimiento();
 	int getNumDeCama();
