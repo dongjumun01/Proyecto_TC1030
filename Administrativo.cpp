56865b36490ef28aa6d6c12f8f84693cdb21fc64
DongjuMun <150094637+DongjuMun@users.noreply.github.com>
2024-05-30 04:08:39 -0600

    Update Administrativo.cpp
    avance 3

--- Administrativo.cpp
+++ Administrativo.cpp
@@ -1,4 +1,4 @@
-﻿#include "Administrativo.h"
+#include "Administrativo.h"
 
 Administrativo::Administrativo()
 {
@@ -6,16 +6,19 @@ Administrativo::Administrativo()
 	password = "";
 	idDePaciente = "";
 	haPagado = false;
+	montoTotal = 0;
 }
-Administrativo::Administrativo(string _userId, string _password) :Empleado(_userId, _password)
+Administrativo::Administrativo(string _userId, string _password, string _nombre, string _tipo) :Empleado(_userId, _password, _nombre, _tipo)
 {
 	idDePaciente = "";
 	haPagado = false;
+	montoTotal = 0;
 }
-Administrativo::Administrativo(string _userId, string _password, string _idDePaciente, bool _haPagado):Empleado(_userId, _password)
+Administrativo::Administrativo(string _userId, string _password, string _nombre, string _tipo, string _idDePaciente, bool _haPagado, float _montoTotal):Empleado(_userId, _password, _nombre, _tipo)
 {
 	idDePaciente = _idDePaciente;
 	haPagado = _haPagado;
+	montoTotal = _montoTotal;
 }
 void Administrativo::setIdDePaciente(string _idDePaciente)
 {
@@ -25,6 +28,10 @@ void Administrativo::setHaPagado(bool _haPagado)
 {
 	haPagado = _haPagado;
 }
+void Administrativo::setMontoTotal(float _montoTotal)
+{
+	montoTotal = _montoTotal;
+}
 string Administrativo::getIdDePaciente()
 {
 	return idDePaciente;
@@ -33,22 +40,66 @@ bool Administrativo::getHaPagado()
 {
 	return haPagado;
 }
-bool Administrativo::checaLogInfo(string _userId, string _password)
+float Administrativo::getMontoTotal()
 {
-	//checa la info de login. Metodo sobreescrito.
+	return montoTotal;
 }
 bool Administrativo::checaIdDePaciente(string _idDePaciente)
 {
 	//checa que si el id del paciete está en el sistema.
-	return true;
-	//si no está en el sistema, regresa falso.
-	//return false;
+	fstream fs;
+	bool haEncontrado = false;
+	string line;
+	fs.open("infoDePago.txt", ios::in);
+	if (!fs) 
+	{ 
+		cout << "Archivo no encontrado" << endl; 
+		return false;
+	}
+	else 
+	{
+		while (!haEncontrado)
+		{
+			if (fs.eof()) { break; }
+			else
+			{
+				getline(fs, line, '\t');
+				if (line == _idDePaciente)
+				{
+					getline(fs, line, '\t'); 
+					montoTotal = stof(line);
+					fs.close();
+					haEncontrado = true;
+					return true;
+				}
+				else { getline(fs, line, '\n'); }
+			}
+		}
+		fs.close();
+		return false;
+	}
 }
-float Administrativo::cargaMontoTotal(string _idDePaciente)
-{
-	//trae el dato del monto total del archivo de texto.
-}
-void Administrativo::guardaInfoDeHaPagado(string _userId, string _password, string _idDePaciente, bool _haPagado)
+void Administrativo::guardaInfoDeHaPagado(string _userId, string _idDePaciente, bool _haPagado, float _montoTotal)
 {
 	//guarda la informacion de que si ha pagado o no con la informacion del administrativo.
+	stringstream ss;
+	ss << _userId << "_" << "pago.txt";
+	fstream fs;
+	fs.open(ss.str(), ios::app);
+	if (!fs) { cout << "Archivo no encontrado" << endl; }
+	else
+	{
+		fs << _idDePaciente << "t";
+		if (_haPagado) { fs << "true" << "t"; }
+		fs << std::to_string(_montoTotal) << "\n";
+		cout << "¡Datos Guardados Exitosamente!" << endl;
+		fs.close();
+	}
+}
+
+string Administrativo::to_string()
+{
+	stringstream ss;
+	ss << "Bienvenido/a, " << nombre << endl;
+	return ss.str();
 }
