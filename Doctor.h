3c01baee117b75e876df3969a581e8c756bec60e
DongjuMun <150094637+DongjuMun@users.noreply.github.com>
2024-06-06 03:20:54 -0600

    Update Doctor.h

--- Doctor.h
+++ Doctor.h
@@ -1,3 +1,8 @@
+/*
+	La clase Doctor es una clase herada de la clase Empleado.
+	Tiene los constructores, getters y setters, y metodos para guardar la informacion de los pacientes de un doctor
+	y para imprimir la informacion del doctor.
+*/
 #pragma once
 #ifndef DOCTOR_H
 #define DOCTOR_H
@@ -8,13 +13,16 @@
 #include "DatoDePaciente.h"
 #include "Empleado.h"
 
+//Declaro clase Doctor que hereda de Empleado
 class Doctor:public Empleado
 {
+//Declaro variables de instancia
 private: 
 	vector<DatoDePaciente> datoDePaciente;
 	int servicio;
+//Declaro los constructores y metodos que va a tener el objeto
 public:
-	Doctor();
+	Doctor(); //sobrecarga
 	Doctor(string _userId, string _password, string _nombre, string _tipo, int _servicio); //sobrecarga
 	Doctor(string _userId, string _password, string _nombre, string _tipo, DatoDePaciente _datoDePaciente, int _servicio); //sobrecarga
 	void setServicio(int _servicio);
@@ -23,7 +31,7 @@ public:
 	void agregarPaciente(DatoDePaciente _datoDePaciente);
 	void guradarInfoDePacientes(string _userId, int _servicio, int _numDePaciente, vector<DatoDePaciente> _datoDePaciente);
 	void guradarInfoDePago(string _userId, int _numDePaciente, vector<DatoDePaciente> _datoDePaciente);
-	string to_string (); //sobreescritura
+	string to_string (); //sobreescrito
 };
 
 #endif
