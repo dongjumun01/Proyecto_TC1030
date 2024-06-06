c42623937fcc9dfd2ab4430572c0af53d2bb8058
DongjuMun <150094637+DongjuMun@users.noreply.github.com>
2024-06-06 03:20:14 -0600

    Update Hospital.h

--- Hospital.h
+++ Hospital.h
@@ -1,3 +1,7 @@
+/*
+	La clase hospital define los objetos de apuntador de la clase abstracta Empelado con el constructor.
+	Puede buscar los empleados y tiene getter de los empleado.
+*/
 #pragma once
 #ifndef HOSPITAL_H
 
@@ -9,12 +13,13 @@ const int MAX = 100;
 
 class Hospital
 {
+//Declaro variables de instancia
 private:
-	Empleado* empleado[MAX] = {0};
+	Empleado* empleado[MAX];//se define como apuntador para usar polimorfismo
 	int index;
-
+//Declaro el constructor y los metodos que va a tener el objeto
 public:
-	Hospital();
+	Hospital(); //crea los objetos en Empleado[]
 	int getIndex();
 	Empleado* getEmpleado(int);
 	bool buscaEmpleado(string, string); //para buscar los datos
