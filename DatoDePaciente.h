7ebe39873d6c268cd281ad105a3e66c066f3bd63
DongjuMun <150094637+DongjuMun@users.noreply.github.com>
2024-06-06 03:21:11 -0600

    Update DatoDePaciente.h

--- DatoDePaciente.h
+++ DatoDePaciente.h
@@ -1,14 +1,20 @@
+/*
+	La clase DatoDePaciente es una clase que esta una relacion de composicion con la clase Doctor.
+	Tiene los constructores, getters y setters, y un metodo para crear el id de paciente.
+*/
 #pragma once
 #ifndef DATODEPACIENTE_H
 #define DATODEPACIENTE_H
 
 #include <iostream>
 #include <algorithm>
+#include <ctype.h>
 
 using namespace std;
-
+//Declaro clase DatoDepaciente
 class DatoDePaciente
 {
+//Declaro variables de instancia
 private:
 	string nombre_doc;
 	string nombre_pac;
@@ -21,10 +27,10 @@ private:
 	bool tratamiento[5];
 	string fechaInicio;
 	string fechaFin;
-
+//Declaro los constructores y metodos que va a tener el objeto
 public:
-	DatoDePaciente();
-	DatoDePaciente(string _nombreDoc, string _nombrePac, string _sexo, string _fechaDeNacimiento, int _numDeCama, string _diagnosis, bool _estudio[], string _sintoma, bool _tratamiento[], string _fechaInicio, string _fechaFin); //overloading
+	DatoDePaciente(); //sobrecarga
+	DatoDePaciente(string _nombreDoc, string _nombrePac, string _sexo, string _fechaDeNacimiento, int _numDeCama, string _diagnosis, bool _estudio[], string _sintoma, bool _tratamiento[], string _fechaInicio, string _fechaFin); //sobrecarga
 	void setNombreDoc(string _nombreDoc);
 	void setNombrePac(string _nombrePac);
 	void setSexo(string _sexo);
