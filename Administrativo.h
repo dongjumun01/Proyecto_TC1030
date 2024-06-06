12dc7179ded8908d0401857cb1507931038490c7
DongjuMun <150094637+DongjuMun@users.noreply.github.com>
2024-06-06 03:21:28 -0600

    Update Administrativo.h

--- Administrativo.h
+++ Administrativo.h
@@ -1,18 +1,25 @@
+/*
+	La clase Administrativo es una clase herada de la clase Empleado.
+	Tiene los constructores, getters y setters, y metodos para guardar la informacion del estado de pago
+	y para imprimir la informacion del adminstrativo.
+*/
 #pragma once
 #ifndef ADMINISTRATIVO_H
 #define ADMINISTRATIVO_H
 
 #include <fstream>
 #include "Empleado.h"
-
+//Declaro clase Administrativo que hereda de Empleado
 class Administrativo:public Empleado
 {
+//Declaro variables de instancia
 private:
 	string idDePaciente;
 	bool haPagado;
 	float montoTotal;
+//Declaro los constructores y metodos que va a tener el objeto
 public:
-	Administrativo();
+	Administrativo(); //sobrecarga
 	Administrativo(string _userId, string _password, string _nombre, string _tipo); //sobrecarga
 	Administrativo(string _userId, string _password, string _nombre, string _tipo, string _idDePaciente, bool _haPagado, float montoTotal); //sobrecarga
 	void setIdDePaciente(string _idDePaciente);
@@ -23,7 +30,7 @@ public:
 	float getMontoTotal();
 	bool checaIdDePaciente(string _idDePaciente);
 	void guardaInfoDeHaPagado(string _userId, string _idDePaciente, bool _haPagado, float _montoTotal);
-	string to_string(); //sobreescritura
+	string to_string(); //sobreescrito
 };
 
 #endif 
