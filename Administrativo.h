53d3e6c2207887f0421512d6ab4f29ba8e443372
dongjumun01 <150094637+dongjumun01@users.noreply.github.com>
2024-08-25 19:37:24 -0600

    Update Administrativo.h

--- Administrativo.h
+++ Administrativo.h
@@ -21,7 +21,8 @@ private:
 public:
 	Administrativo(); //sobrecarga
 	Administrativo(string _userId, string _password, string _nombre, string _tipo); //sobrecarga
-	Administrativo(string _userId, string _password, string _nombre, string _tipo, string _idDePaciente, bool _haPagado, float montoTotal); //sobrecarga
+	Administrativo(string _userId, string _password, string _nombre, string _tipo, 
+		       string _idDePaciente, bool _haPagado, float montoTotal); //sobrecarga
 	void setIdDePaciente(string _idDePaciente);
 	void setHaPagado(bool _haPagado);
 	void setMontoTotal(float _montoTotal);
@@ -29,7 +30,8 @@ public:
 	bool getHaPagado();
 	float getMontoTotal();
 	bool checaIdDePaciente(string _idDePaciente);
-	void guardaInfoDeHaPagado(string _userId, string _idDePaciente, bool _haPagado, float _montoTotal);
+	void guardaInfoDeHaPagado(string _userId, string _idDePaciente, 
+                                  bool _haPagado, float _montoTotal);
 	string to_string(); //sobreescrito
 };
 
