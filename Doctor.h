21ba647a8b8a0e7a9ff23b1ebba3eddaf2ee0fe2
dongjumun01 <150094637+dongjumun01@users.noreply.github.com>
2024-08-25 19:48:55 -0600

    Update Doctor.h

--- Doctor.h
+++ Doctor.h
@@ -1,6 +1,7 @@
 /*
 	La clase Doctor es una clase herada de la clase Empleado.
-	Tiene los constructores, getters y setters, y metodos para guardar la informacion de los pacientes de un doctor
+	Tiene los constructores, getters y setters, y metodos para 
+ 	guardar la informacion de los pacientes de un doctor
 	y para imprimir la informacion del doctor.
 */
 #pragma once
@@ -23,14 +24,20 @@ private:
 //Declaro los constructores y metodos que va a tener el objeto
 public:
 	Doctor(); //sobrecarga
-	Doctor(string _userId, string _password, string _nombre, string _tipo, int _servicio); //sobrecarga
-	Doctor(string _userId, string _password, string _nombre, string _tipo, DatoDePaciente _datoDePaciente, int _servicio); //sobrecarga
+	Doctor(string _userId, string _password, string _nombre, 
+	       string _tipo, int _servicio); //sobrecarga
+	Doctor(string _userId, string _password, string _nombre, 
+               string _tipo, DatoDePaciente _datoDePaciente, 
+               int _servicio); //sobrecarga
 	void setServicio(int _servicio);
 	int getServicio();
 	vector<DatoDePaciente>& getDatoDePaciente();
 	void agregarPaciente(DatoDePaciente _datoDePaciente);
-	void guradarInfoDePacientes(string _userId, int _servicio, int _numDePaciente, vector<DatoDePaciente> _datoDePaciente);
-	void guradarInfoDePago(string _userId, int _numDePaciente, vector<DatoDePaciente> _datoDePaciente);
+	void guradarInfoDePacientes(string _userId, int _servicio, 
+                                    int _numDePaciente, 
+                                    vector<DatoDePaciente> _datoDePaciente);
+	void guradarInfoDePago(string _userId, int _numDePaciente, 
+			       vector<DatoDePaciente> _datoDePaciente);
 	string to_string (); //sobreescrito
 };
 
